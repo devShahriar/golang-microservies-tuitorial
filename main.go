56d09049936fc59c7160c0fa7331edb6773d7bac
Shahriar-shudip <shudip.shahriar@gmail.com>
2021-01-10 23:15:11 +0600

    -

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/mico/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		log.Println("hellp")
-		data, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			http.Error(w, "Opsss", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(w, "Hello %s", data)
-	})
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, req *http.Request) {
-		log.Println("hello")
-		data, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			http.Error(w, "Opsss", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(w, "Hello%s", data)
-	})
+	l := log.New(os.Stdout, "api", log.LstdFlags)
+	handler := handlers.NewRouter(l)
 
-	http.ListenAndServe(":9000", nil)
+	sh := http.NewServeMux()
+	sh.Handle("/", handler)
+	http.ListenAndServe(":9000", sh)
 
 }
