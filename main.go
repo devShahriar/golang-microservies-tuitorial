b70af15039fa9530239914348899f99d8edffc47
Shahriar-shudip <shudip.shahriar@gmail.com>
2021-01-10 23:59:27 +0600

    -

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/mico/handlers"
 )
@@ -13,8 +16,29 @@ func main() {
 	l := log.New(os.Stdout, "api", log.LstdFlags)
 	handler := handlers.NewRouter(l)
 
-	sh := http.NewServeMux()
-	sh.Handle("/", handler)
-	http.ListenAndServe(":9000", sh)
+	sm := http.NewServeMux()
+	sm.Handle("/", handler)
 
+	server := http.Server{
+		Addr:         ":9000",
+		Handler:      sm,
+		IdleTimeout:  123 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	log.Println("gracful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(tc) // shuts the server when users has done with the request
 }
